1b09a74e9084a58ac6adbbe6b64038804f682186
Jack Oien <oien0034@d.umn.edu>
2026-09-04 11:19:23 -0500

    Simplified sphere rendering

--- src/main.cc
+++ src/main.cc
@@ -6,16 +6,16 @@
 
 float hit_sphere(const point3& center, float radius, const ray& r) {
     vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = h*h - a*c;
     // return (discriminant >= 0 );
 
     if (discriminant < 0) {
         return -1.0;
     } else {
-        return (-b - std::sqrt(discriminant) ) / (2.0*a);
+        return (h - std::sqrt(discriminant)) / a;
     }
 }
 
